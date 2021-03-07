77d8020d05b8c8a46e0049cc299d1e82ed6c03d8
Javad-Forough <javadforough@gmail.com>
2021-03-07 18:18:11 +0330

    Add reading the data from Data.txt

--- ccfd.py
+++ ccfd.py
@@ -6,3 +6,7 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import auc, average_precision_score, recall_score, precision_score, accuracy_score, classification_report, precision_recall_curve,f1_score
 from matplotlib import pyplot
+
+# Reading the data
+with open("Dataset/Data.txt", "rb") as fp:
+    data_list = pickle.load(fp)
