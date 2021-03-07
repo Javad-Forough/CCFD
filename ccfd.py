646e28adb95485d80e87afc0fc19fa3ef5480377
Javad-Forough <javadforough@gmail.com>
2021-03-07 21:35:05 +0330

    Extract method predict_probs

--- ccfd.py
+++ ccfd.py
@@ -74,7 +74,8 @@ class Classifier:
         self.train(self.x_train, self.x_val, self.y_train, self.y_val)
         # predicting the class of each test instances
         self.predict_class(self.x_test)
-
+        # predicting the probability for each test instances
+        self.predict_probs(self.x_test)
 
     def build(self):
         self.ann_model = Sequential()
@@ -91,7 +92,10 @@ class Classifier:
 
     def predict_class(self, x_test):
         predictions = self.ann_model.predict_classes(x_test)
-        y_hat = predictions[:, 0]
+        self.y_hat = predictions[:, 0]
+
+    def predict_probs(self, x_test):
+        self.ann_probs = self.ann_model.predict(x_test)[:, 0]
 
 
 
@@ -100,8 +104,7 @@ class Classifier:
 
 
 
-# predicting the probability for each test instances
-ann_probs = ann_model.predict(x_test)[:, 0]
+
 
 # Evaluating the model using Precision, Recall, and F1
 precision = precision_score(y_test, y_hat)
